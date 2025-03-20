966c828dabd20a901cd04c4cc9f8e56d7d749f21
Takuya Shizukuishi <sititou70@gmail.com>
2025-03-20 13:54:34 +0900

    refactor: normal

--- normal.inline.js
+++ normal.inline.js
@@ -6,80 +6,17 @@
 		)(
 			console.log(
 				((n) =>
-					((n) => n(() => () => (dfval) => dfval())((dtval) => () => dtval()))(
-						((g) => ((h) => g((n) => h(h)(n)))((h) => g((n) => h(h)(n))))(
-							(mod) => (n1) => (n2) =>
-								(
-									(n1) => (n2) =>
-										((n) =>
-											n(() => () => (dfval) => dfval())(
-												(dtval) => () => dtval(),
-											))(
-											(
-												(n1) => (n2) =>
-													n2((n) =>
-														((pair) => pair(() => (cdr) => cdr))(
-															n((pair) =>
-																(
-																	(car) => (cdr) => (selector) =>
-																		selector(car)(cdr)
-																)(
-																	(
-																		(n) => (s) => (z) =>
-																			s(n(s)(z))
-																	)(((pair) => pair((car) => () => car))(pair)),
-																)(((pair) => pair((car) => () => car))(pair)),
-															)(
-																(
-																	(car) => (cdr) => (selector) =>
-																		selector(car)(cdr)
-																)((s) => (z) => z)((s) => (z) => z),
-															),
-														),
-													)(n1)
-											)(
-												(
-													(n) => (s) => (z) =>
-														s(n(s)(z))
-												)(n1),
-											)(n2),
-										)
-								)(n1)(n2)(() => n1)(() =>
-									mod(
-										(
-											(n1) => (n2) =>
-												n2((n) =>
-													((pair) => pair(() => (cdr) => cdr))(
-														n((pair) =>
-															(
-																(car) => (cdr) => (selector) =>
-																	selector(car)(cdr)
-															)(
-																(
-																	(n) => (s) => (z) =>
-																		s(n(s)(z))
-																)(((pair) => pair((car) => () => car))(pair)),
-															)(((pair) => pair((car) => () => car))(pair)),
-														)(
-															(
-																(car) => (cdr) => (selector) =>
-																	selector(car)(cdr)
-															)((s) => (z) => z)((s) => (z) => z),
-														),
-													),
-												)(n1)
-										)(n1)(n2),
-									)(n2),
-								),
-						)(n)(
+					(
+						(isMultOf3) => (isMultOf5) =>
 							(
-								(n1) => (n2) =>
-									n1((n) => n2((n) => (s) => (z) => s(n(s)(z)))(n))(
-										(s) => (z) => z,
-									)
-							)((s) => (z) => s(s(s(z))))((s) => (z) => s(s(s(s(s(z)))))),
-						),
-					)(() => "Fizz Buzz")(() =>
+								(b1) => (b2) =>
+									b1(() => b2)(() => () => (dfval) => dfval())
+							)(isMultOf3)(isMultOf5)(() => "Fizz Buzz")(() =>
+								isMultOf3(() => "Fizz")(() =>
+									isMultOf5(() => "Buzz")(() => ((n) => n((x) => x + 1)(0))(n)),
+								),
+							)
+					)(
 						((n) =>
 							n(() => () => (dfval) => dfval())((dtval) => () => dtval()))(
 							((g) => ((h) => g((n) => h(h)(n)))((h) => g((n) => h(h)(n))))(
@@ -151,56 +88,18 @@
 										)(n2),
 									),
 							)(n)((s) => (z) => s(s(s(z)))),
-						)(() => "Fizz")(() =>
-							((n) =>
-								n(() => () => (dfval) => dfval())((dtval) => () => dtval()))(
-								((g) => ((h) => g((n) => h(h)(n)))((h) => g((n) => h(h)(n))))(
-									(mod) => (n1) => (n2) =>
-										(
-											(n1) => (n2) =>
-												((n) =>
-													n(() => () => (dfval) => dfval())(
-														(dtval) => () => dtval(),
-													))(
-													(
-														(n1) => (n2) =>
-															n2((n) =>
-																((pair) => pair(() => (cdr) => cdr))(
-																	n((pair) =>
-																		(
-																			(car) => (cdr) => (selector) =>
-																				selector(car)(cdr)
-																		)(
-																			(
-																				(n) => (s) => (z) =>
-																					s(n(s)(z))
-																			)(
-																				((pair) => pair((car) => () => car))(
-																					pair,
-																				),
-																			),
-																		)(
-																			((pair) => pair((car) => () => car))(
-																				pair,
-																			),
-																		),
-																	)(
-																		(
-																			(car) => (cdr) => (selector) =>
-																				selector(car)(cdr)
-																		)((s) => (z) => z)((s) => (z) => z),
-																	),
-																),
-															)(n1)
-													)(
-														(
-															(n) => (s) => (z) =>
-																s(n(s)(z))
-														)(n1),
-													)(n2),
-												)
-										)(n1)(n2)(() => n1)(() =>
-											mod(
+						),
+					)(
+						((n) =>
+							n(() => () => (dfval) => dfval())((dtval) => () => dtval()))(
+							((g) => ((h) => g((n) => h(h)(n)))((h) => g((n) => h(h)(n))))(
+								(mod) => (n1) => (n2) =>
+									(
+										(n1) => (n2) =>
+											((n) =>
+												n(() => () => (dfval) => dfval())(
+													(dtval) => () => dtval(),
+												))(
 												(
 													(n1) => (n2) =>
 														n2((n) =>
@@ -227,11 +126,41 @@
 																),
 															),
 														)(n1)
-												)(n1)(n2),
-											)(n2),
-										),
-								)(n)((s) => (z) => s(s(s(s(s(z)))))),
-							)(() => "Buzz")(() => ((n) => n((x) => x + 1)(0))(n)),
+												)(
+													(
+														(n) => (s) => (z) =>
+															s(n(s)(z))
+													)(n1),
+												)(n2),
+											)
+									)(n1)(n2)(() => n1)(() =>
+										mod(
+											(
+												(n1) => (n2) =>
+													n2((n) =>
+														((pair) => pair(() => (cdr) => cdr))(
+															n((pair) =>
+																(
+																	(car) => (cdr) => (selector) =>
+																		selector(car)(cdr)
+																)(
+																	(
+																		(n) => (s) => (z) =>
+																			s(n(s)(z))
+																	)(((pair) => pair((car) => () => car))(pair)),
+																)(((pair) => pair((car) => () => car))(pair)),
+															)(
+																(
+																	(car) => (cdr) => (selector) =>
+																		selector(car)(cdr)
+																)((s) => (z) => z)((s) => (z) => z),
+															),
+														),
+													)(n1)
+											)(n1)(n2),
+										)(n2),
+									),
+							)(n)((s) => (z) => s(s(s(s(s(z)))))),
 						),
 					))(n),
 			),
@@ -244,11 +173,6 @@
 	)((s) => (z) => s(z)))(
 	(
 		(n1) => (n2) =>
-			n1((n) => n2((n) => (s) => (z) => s(n(s)(z)))(n))((s) => (z) => z)
-	)(
-		(
-			(n1) => (n2) =>
-				n1((n) => n2((n) => (s) => (z) => s(n(s)(z)))(n))((s) => (z) => z)
-		)((s) => (z) => s(s(s(s(s(z))))))((s) => (z) => s(s(s(s(s(z)))))),
-	)((s) => (z) => s(s(s(s(s(z)))))),
+			n2(n1)
+	)((s) => (z) => s(s(s(s(z)))))((s) => (z) => s(s(s(s(z))))),
 );
--- normal.js
+++ normal.js
@@ -5,7 +5,6 @@ export const _three = (s) => (z) => s(s(s(z)));
 export const _five = (s) => (z) => s(s(s(s(s(z)))));
 
 export const _succ = (n) => (s) => (z) => s(n(s)(z));
-export const _mul = (n1) => (n2) => n1((n) => n2(_succ)(n))(_zero);
 
 export const _cons = (car) => (cdr) => (selector) => selector(car)(cdr);
 export const _car = (pair) => pair((car) => () => car);
@@ -18,43 +17,49 @@ export const _sub = (n1) => (n2) => n2(_pred)(n1);
 export const _true = (dtval) => () => dtval();
 export const _false = () => (dfval) => dfval();
 
+export const _and = (b1) => (b2) => b1(() => b2)(() => _false);
+
 export const _isZero = (n) => n(() => _false)(_true);
 export const _lessThan = (n1) => (n2) => _isZero(_sub(_succ(n1))(n2));
 
 export const _fix = (g) => ((h) => g((n) => h(h)(n)))((h) => g((n) => h(h)(n)));
-
 export const _mod = _fix(
 	(mod) => (n1) => (n2) =>
 		_lessThan(n1)(n2)(() => n1)(() => mod(_sub(n1)(n2))(n2)),
 );
 
 export const _fizzBuzz = (n) =>
-	_isZero(_mod(n)(_mul(_three)(_five)))(
-		// n % (3 * 5) === 0
-		() => "Fizz Buzz",
-	)(
-		// n % (3 * 5) !== 0
-		() =>
-			_isZero(_mod(n)(_three))(
-				// n % 3 === 0
-				() => "Fizz",
+	(
+		(isMultOf3) => (isMultOf5) =>
+			_and(isMultOf3)(isMultOf5)(
+				// n % (3 * 5) === 0
+				() => "Fizz Buzz",
 			)(
-				// n % 3 !== 0
+				// n % (3 * 5) !== 0
 				() =>
-					_isZero(_mod(n)(_five))(
-						// n % 5 === 0
-						() => "Buzz",
+					isMultOf3(
+						// n % 3 === 0
+						() => "Fizz",
 					)(
-						// n % 5 !== 0
-						() => _toNumber(n),
+						// n % 3 !== 0
+						() =>
+							isMultOf5(
+								// n % 5 === 0
+								() => "Buzz",
+							)(
+								// n % 5 !== 0
+								() => _toNumber(n),
+							),
 					),
-			),
-	);
+			)
+	)(_isZero(_mod(n)(_three)))(_isZero(_mod(n)(_five)));
 
 export const _one = (s) => (z) => s(z);
+export const _four = (s) => (z) => s(s(s(s(z))));
+export const _exp = (n1) => (n2) => n2(n1);
 export const _seq = (proc1) => (proc2) => proc2;
 export const _printFizzBuzzList = (printNum) =>
 	printNum((n) => _seq(console.log(_fizzBuzz(n)))(_succ(n)))(_one);
 
-// print fizz buzz list to 125 = 5 * 5 * 5
-_printFizzBuzzList(_mul(_mul(_five)(_five))(_five));
+// print fizz buzz list to 256 = 4 ^ 4
+_printFizzBuzzList(_exp(_four)(_four));
